a05068ff3789735f1c810432d14d3cc6c3f7507a
Sean Welch <seanw51199@gmail.com>
2023-10-15 16:21:03 +0100

    added src and etc dir

--- src/infrastructure/__main__.py
+++ src/infrastructure/__main__.py
@@ -1,36 +1,5 @@
 import json
-import pulumi
 import pulumi_aws as aws
-
-default_vpc = aws.ec2.DefaultVpc("default-vpc", tags={"Name": "Default VPC"})
-
-default_az1 = aws.ec2.DefaultSubnet(
-    "default-az-1",
-    availability_zone="eu-west-1a",
-    tags={
-        "Name": "Default subnet for eu-west-1a",
-    },
-)
-
-default_az2 = aws.ec2.DefaultSubnet(
-    "default-az-2",
-    availability_zone="eu-west-1b",
-    tags={
-        "Name": "Default subnet for eu-west-1b",
-    },
-)
-
-default_az3 = aws.ec2.DefaultSubnet(
-    "default-az-3",
-    availability_zone="eu-west-1c",
-    tags={
-        "Name": "Default subnet for eu-west-1c",
-    },
-)
-
-subnet_ids = pulumi.Output.all(default_az1.id, default_az2.id, default_az3.id).apply(
-    lambda az: f"{az[0]},{az[1]},{az[2]}"
-)
 
 instance_profile_role = aws.iam.Role(
     "eb-ec2-role",
@@ -73,17 +42,14 @@ eb_env = aws.elasticbeanstalk.Environment(
     solution_stack_name="64bit Amazon Linux 2023 v4.0.1 running Docker",
     settings=[
         aws.elasticbeanstalk.EnvironmentSettingArgs(
+            namespace="aws:elasticbeanstalk:environment:proxy",
+            name="ProxyServer",
+            value="nginx",
+        ),
+        aws.elasticbeanstalk.EnvironmentSettingArgs(
             namespace="aws:autoscaling:launchconfiguration",
             name="IamInstanceProfile",
             value=instance_profile.name,
-        ),
-        aws.elasticbeanstalk.EnvironmentSettingArgs(
-            namespace="aws:ec2:vpc",
-            name="VPCId",
-            value=default_vpc.id,
-        ),
-        aws.elasticbeanstalk.EnvironmentSettingArgs(
-            namespace="aws:ec2:vpc", name="Subnets", value=subnet_ids
         ),
     ],
 )
