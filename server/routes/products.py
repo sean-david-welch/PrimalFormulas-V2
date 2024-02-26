598f6de1665951e5fa692f255ce749d82c9ca47f
Sean Welch <seanwelch51199@icloud.com>
2024-02-26 21:56:37 +0000

    product routes

--- server/routes/products.py
+++ server/routes/products.py
@@ -15,3 +15,43 @@ async def get_products():
         return {"Error": error.detail}, error.status_code
 
     return response
+
+
+@router.get("/{id}", response_model=Product)
+async def get_product_by_id(id: str):
+    try:
+        response = await database.get_product_by_id(id)
+    except HTTPException as error:
+        return {"Error": error.detail}, error.status_code
+
+    return response
+
+
+@router.post("/", response_model=Product)
+async def create_product(product: Product):
+    try:
+        response = await database.create_product(product)
+    except HTTPException as error:
+        return {"Error": error.detail}, error.status_code
+
+    return response
+
+
+@router.put("/{id}", response_model=Product)
+async def update_product(id: str, product: Product):
+    try:
+        response = await database.update_product(id, product)
+    except HTTPException as error:
+        return {"Error": error.detail}, error.status_code
+
+    return response
+
+
+@router.delete("/{id}", response_model=Product)
+async def delete_product(id: str, product: Product):
+    try:
+        response = await database.delete_product(id, product)
+    except HTTPException as error:
+        return {"Error": error.detail}, error.status_code
+
+    return response
