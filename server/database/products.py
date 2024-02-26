3af190f85dc1e3da5a143fd06d4017fc01b1b652
Sean Welch <seanwelch51199@icloud.com>
2024-02-26 21:28:19 +0000

    product database queries

--- server/database/products.py
+++ server/database/products.py
@@ -1,5 +1,8 @@
 import logging
 
+from uuid import uuid4
+from datetime import datetime
+
 from models.models import Product
 from utils.database import get_async_pool
 
@@ -11,9 +14,9 @@ async def get_products() -> list[Product]:
     query = "SELECT * FROM products"
 
     try:
-        async with pool.connection() as conn, conn.cursor() as curr:
-            curr.execute(query)
-            rows = curr.fetchall()
+        async with pool.connection() as conn, conn.cursor() as cursor:
+            await cursor.execute(query)
+            rows = await cursor.fetchall()
 
             return [
                 Product(
@@ -28,4 +31,77 @@ async def get_products() -> list[Product]:
             ]
     except Exception as error:
         logger.error(f"An error occurred in get_products: {error}", exc_info=True)
+        return None
+
+
+async def get_product_by_id(id: str) -> Product:
+    query = "SELECT * FROM products WHERE id = %s"
+
+    try:
+        async with pool.connection() as conn, conn.cursor() as cursor:
+            await cursor.execute(query, (id))
+            product = await cursor.fetchone()
+
+            return Product(*product) if product != None else None
+    except Exception as error:
+        logger.error(f"An error occurred in get_products_by_id: {error}", exc_info=True)
+        return None
+
+
+async def create_product(product: Product) -> bool:
+    product.id = uuid4()
+    product.created = datetime.now()
+
+    query = "INSERT INTO products (id, name, description, price, image, created) VALUES (%s, %s, %s, %s, %s, %s)"
+    values = (
+        product.id,
+        product.name,
+        product.description,
+        product.price,
+        product.image,
+        product.created,
+    )
+
+    try:
+        async with pool.connection() as conn, conn.transaction():
+            await conn.execute(query, values)
+            return True
+    except Exception as error:
+        logger.error(f"an error occurred while creating a product: {error}")
+        return None
+
+
+from uuid import UUID
+from datetime import datetime
+
+
+async def update_product(id: str, product: Product) -> bool:
+    query = "UPDATE products SET name = %s, description = %s, price = %s, image = %s WHERE id = %s"
+    values = (
+        product.name,
+        product.description,
+        product.price,
+        product.image,
+        id,
+    )
+
+    try:
+        async with pool.connection() as conn, conn.transaction():
+            await conn.execute(query, values)
+            return True
+    except Exception as error:
+        logger.error(f"an error occurred while updating a product: {error}")
+        return None
+
+
+async def delete_product(id: str) -> bool:
+    query = "DELETE FROM products WHERE id = %s"
+
+    try:
+        async with pool.connection() as conn, conn.transaction():
+            await conn.execute(query(id))
+
+            return True
+    except Exception as error:
+        logger.error(f"an error occurred while deleting a product: {error}")
         return None
