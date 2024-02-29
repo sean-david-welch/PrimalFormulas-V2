88acae1fe671df65fc0596c526341c2404c8d416
Sean Welch <seanwelch51199@icloud.com>
2024-02-29 08:48:43 +0000

    going to do terraform

--- server/lib/firebase.py
+++ server/lib/firebase.py
@@ -1,7 +1,7 @@
 import json
-import firebase_admin
-from firebase_admin import credentials
+
 from utils.config import settings
+from firebase_admin import credentials, initialize_app
 
 service_account_info = settings["SERVICE_ACCOUNT_STRING"]
 
@@ -9,6 +9,6 @@ if service_account_info is not None:
     service_account_json = json.loads(service_account_info)
 
     cred = credentials.Certificate(service_account_json)
-    firebase_admin.initialize_app(cred)
+    initialize_app(cred)
 else:
     print("environment variable not found.")
