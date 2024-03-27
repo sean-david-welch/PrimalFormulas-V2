3c7767dd204fb690217cb53368595aa6c296d4fc
Sean Welch <seanwelch51199@icloud.com>
2024-03-27 15:39:25 +0000

    some url config

--- server/primalformulas/urls.py
+++ server/primalformulas/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("products/", include("products.urls")),
+    path("assets/", include("assets.urls")),
+    path("about/", include("about.urls")),
 ]
