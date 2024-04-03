14ec5d8dd3b4f83dbe67813727b424a0ab4c51ae
Sean Welch <seanwelch51199@icloud.com>
2024-04-03 19:52:36 +0100

    some more bootstrapps

---
+++ client/src/app/components/assets/assets.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-assets',
+  standalone: true,
+  imports: [],
+  templateUrl: './assets.component.html',
+  styleUrl: './assets.component.css'
+})
+export class AssetsComponent {
+
+}
--- client/src/app/services/auth/auth.service.spec.ts
+++ client/src/app/services/auth/auth.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { CartServiceService } from './cart-service.service';
+import { AuthService } from './auth.service';
 
-describe('CartServiceService', () => {
-  let service: CartServiceService;
+describe('AuthService', () => {
+  let service: AuthService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(CartServiceService);
+    service = TestBed.inject(AuthService);
   });
 
   it('should be created', () => {
