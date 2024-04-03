03466bbe156a858ba33a993be632794602a22feb
Sean Welch <seanwelch51199@icloud.com>
2024-04-03 11:54:48 +0100

    added images

---
+++ client/src/app/components/products-list/products-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './products-list.component.html',
+  styleUrl: './products-list.component.css'
+})
+export class ProductsListComponent {
+
+}
