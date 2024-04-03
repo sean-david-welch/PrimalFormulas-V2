a18663a2afaddcb289c963cbd6b54707751e1663
Sean Welch <seanwelch51199@icloud.com>
2024-04-03 14:36:20 +0100

    bootstrapped all components i think

---
+++ client/src/app/components/nav-button/nav-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './nav-button.component.html',
+  styleUrl: './nav-button.component.css'
+})
+export class NavButtonComponent {
+
+}
