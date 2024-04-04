a329bc126cefaf92d303f2bd02a23c3c661ab5a9
Sean Welch <seanwelch51199@icloud.com>
2024-04-04 10:33:02 +0100

    added sidebar

--- client/src/app/components/navbar/navbar.component.ts
+++ client/src/app/components/navbar/navbar.component.ts
@@ -1,22 +1,26 @@
 import { Component, Input } from '@angular/core';
 import { NavigationStart, Router } from '@angular/router';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
 import {
-  IconDefinition,
-  faArrowRight,
+  faCartShopping,
+  faUserCircle,
 } from '@fortawesome/free-solid-svg-icons';
-import { navbarLinkIcons } from './navbar.constants';
+
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NavButtonComponent } from '../nav-button/nav-button.component';
+import { SidebarComponent } from '../sidebar/sidebar.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [FontAwesomeModule, NavButtonComponent],
+  imports: [FontAwesomeModule, NavButtonComponent, SidebarComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
   public isHomePage: boolean = true;
+  public cartShoppingIcon: IconDefinition = faCartShopping;
+  public userCircleIcon: IconDefinition = faUserCircle;
 
   constructor(private router: Router) {
     router.events.subscribe((event) => {
@@ -24,10 +28,5 @@ export class NavbarComponent {
         this.isHomePage = event.url === '/';
       }
     });
-  }
-
-  @Input() iconName: string = '';
-  get icon(): IconDefinition {
-    return navbarLinkIcons[this.iconName] || faArrowRight;
   }
 }
--- client/src/app/components/sidebar/sidebar.component.ts
+++ client/src/app/components/sidebar/sidebar.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { NavLogoComponent } from '../nav-logo/nav-logo.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { RouterModule } from '@angular/router';
+import {
+  faBars,
+  faCartShopping,
+  faUserCircle,
+  faX,
+} from '@fortawesome/free-solid-svg-icons';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [NavLogoComponent, FontAwesomeModule, RouterModule, CommonModule],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css'
+  styleUrl: './sidebar.component.css',
 })
 export class SidebarComponent {
+  faBars = faBars;
+  faX = faX;
+  faShopping = faCartShopping;
+  faUserCircle = faUserCircle;
 
+  private open = new BehaviorSubject<boolean>(false);
+
+  public open$ = this.open.asObservable();
+
+  public toggleSidebar() {
+    this.open.next(!this.open.value);
+  }
 }
