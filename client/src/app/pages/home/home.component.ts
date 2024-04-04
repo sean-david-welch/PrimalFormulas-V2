e3cac0512cf68bda1f36a67badb9b75500579dc6
Sean Welch <seanwelch51199@icloud.com>
2024-04-04 15:22:39 +0100

    added some components across

--- client/src/app/pages/home/home.component.ts
+++ client/src/app/pages/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { HeroComponent } from '../../components/hero/hero.component';
+import { DisplaysComponent } from '../../components/displays/displays.component';
+import { ProductsListComponent } from '../../components/products-list/products-list.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HeroComponent, DisplaysComponent, ProductsListComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
