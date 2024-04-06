27bbc3c6caa5bb929851e8ddf3fcd504df0221ae
Sean Welch <seanwelch51199@icloud.com>
2024-04-06 21:03:15 +0100

    rendered ui based on user

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -6,18 +6,16 @@ import { ProductDetailComponent } from './pages/product-detail/product-detail.co
 import { AccountComponent } from './pages/account/account.component';
 import { CartComponent } from './pages/cart/cart.component';
 import { CheckoutComponent } from './pages/checkout/checkout.component';
-import { LoginComponent } from './pages/login/login.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'products', component: ProductsComponent },
-  { path: 'products/:id', component: ProductDetailComponent },
-  { path: 'cart', component: CartComponent },
-  { path: 'checkout', component: CheckoutComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'account', component: AccountComponent },
-  { path: 'not-found', component: NotFoundComponent },
-  { path: '**', redirectTo: '/not-found' },
+    { path: '', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'products', component: ProductsComponent },
+    { path: 'products/:id', component: ProductDetailComponent },
+    { path: 'cart', component: CartComponent },
+    { path: 'checkout', component: CheckoutComponent },
+    { path: 'account', component: AccountComponent },
+    { path: 'not-found', component: NotFoundComponent },
+    { path: '**', redirectTo: '/not-found' },
 ];
--- client/src/app/pages/account/account.component.ts
+++ client/src/app/pages/account/account.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { NavLogoComponent } from '../../components/nav-logo/nav-logo.component';
 import { NavButtonComponent } from '../../components/nav-button/nav-button.component';
 import { LoginFormComponent } from '../../forms/login-form/login-form.component';
+import { AuthService } from '../../services/auth/auth.service';
+import { LoginService } from '../../services/login/login.service';
+import { User } from '../../models/models';
 
 @Component({
     selector: 'app-account',
@@ -11,5 +14,13 @@ import { LoginFormComponent } from '../../forms/login-form/login-form.component'
     styleUrl: './account.component.css'
 })
 export class AccountComponent {
+    public user: User | null = null;
 
+    public authService = inject(AuthService)
+    public loginService = inject(LoginService)
+
+    ngOnInit(): void {
+        this.authService.loadFromLocalStorage();
+        this.user = this.authService.user();
+    }
 }
