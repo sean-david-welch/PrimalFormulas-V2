ef57185de21826db9235c6997485875e413e52ed
seanwelch <seanwelch51199@icloud.com>
2024-05-06 17:28:28 +0100

    finished about database implementations

--- server/api/database/about.go
+++ server/api/database/about.go
@@ -42,3 +42,91 @@ func (store *AboutStoreImpl) GetAbouts() ([]*types.About, error) {
 
 	return abouts, nil
 }
+
+func (store *AboutStoreImpl) GetAboutByID(id string) (*types.About, error) {
+	input := &dynamodb.GetItemInput{TableName: aws.String(TABLE_NAME), Key: map[string]*dynamodb.AttributeValue{
+		"id": {
+			S: aws.String(id),
+		},
+	}}
+
+	result, err := store.database.Database.GetItem(input)
+	if err != nil {
+		return nil, err
+	}
+
+	if result.Item == nil {
+		return nil, nil
+	}
+
+	var about *types.About
+	err = dynamodbattribute.UnmarshalMap(result.Item, &about)
+	if err != nil {
+		return nil, err
+	}
+
+	return about, nil
+}
+
+func (store *AboutStoreImpl) CreateAbout(about *types.About) (*types.About, error) {
+	item, err := dynamodbattribute.MarshalMap(about)
+	if err != nil {
+		return nil, err
+	}
+
+	input := &dynamodb.PutItemInput{
+		TableName: aws.String(TABLE_NAME),
+		Item:      item,
+	}
+
+	_, err = store.database.Database.PutItem(input)
+	if err != nil {
+		return nil, err
+	}
+
+	return about, nil
+}
+
+func (store *AboutStoreImpl) UpdateAbout(about *types.About) (*types.About, error) {
+	updateExpression := "SET Title = :title, Description = : desc, Image = : img"
+	input := &dynamodb.UpdateItemInput{
+		TableName: aws.String(TABLE_NAME),
+		Key: map[string]*dynamodb.AttributeValue{
+			"id": {
+				S: aws.String(about.ID),
+			},
+		},
+		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+			":title":       {S: aws.String(about.Title)},
+			":description": {S: aws.String(about.Description)},
+			":img":         {S: aws.String(about.Image)},
+		},
+		UpdateExpression: aws.String(updateExpression),
+		ReturnValues:     aws.String("UPDATED_NEW"),
+	}
+
+	_, err := store.database.Database.UpdateItem(input)
+	if err != nil {
+		return nil, err
+	}
+
+	return about, nil
+}
+
+func (store *AboutStoreImpl) DeleteAbout(about *types.About) error {
+	input := &dynamodb.DeleteItemInput{
+		TableName: aws.String(TABLE_NAME),
+		Key: map[string]*dynamodb.AttributeValue{
+			"id": {
+				S: aws.String(about.ID),
+			},
+		},
+	}
+
+	_, err := store.database.Database.DeleteItem(input)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
