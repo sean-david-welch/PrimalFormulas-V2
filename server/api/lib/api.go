869edd4381fe08b041ab18e2c924331e92a45733
seanwelch <seanwelch51199@icloud.com>
2024-05-12 16:03:27 +0100

    generic response handlers

--- server/api/lib/api.go
+++ server/api/lib/api.go
@@ -1,3 +1,36 @@
 package lib
 
-func
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/aws/aws-lambda-go/events"
+	"net/http"
+)
+
+type ResponseHandler[T any] interface {
+	ErrorResponse(err error, statusCode int) *events.APIGatewayProxyResponse
+	SuccessResponse(data interface{}) *events.APIGatewayProxyResponse
+}
+
+type ResponseHandlerImpl[T any] struct{}
+
+func (response *ResponseHandlerImpl[T]) ErrorResponse(err error, statusCode int) *events.APIGatewayProxyResponse {
+	return &events.APIGatewayProxyResponse{
+		StatusCode: statusCode,
+		Body:       err.Error(),
+	}
+}
+
+func (response *ResponseHandlerImpl[T]) SuccessResponse(data interface{}) *events.APIGatewayProxyResponse {
+	body, err := json.Marshal(data)
+	if err != nil {
+		return response.ErrorResponse(
+			fmt.Errorf("failed to marshal data: %v", err), http.StatusInternalServerError)
+	}
+
+	return &events.APIGatewayProxyResponse{
+		StatusCode: http.StatusOK,
+		Headers:    map[string]string{"Content-Type": "application/json"},
+		Body:       string(body),
+	}
+}
