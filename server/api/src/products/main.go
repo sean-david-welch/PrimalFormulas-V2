dbfe224f8b9efdea34e60d9548d176d832913e7e
seanwelch <seanwelch51199@icloud.com>
2024-05-15 22:04:16 +0100

    product lambda

---
+++ server/api/src/products/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/sean-david-welch/primal-formulas/config"
+	"github.com/sean-david-welch/primal-formulas/database"
+	"github.com/sean-david-welch/primal-formulas/handlers"
+	"github.com/sean-david-welch/primal-formulas/lib"
+	"github.com/sean-david-welch/primal-formulas/services"
+	"log"
+	"net/http"
+)
+
+func main() {
+	lambda.Start(Handler)
+}
+
+func Handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	secrets, err := config.NewSecrets()
+	if err != nil {
+		log.Fatalf("Error loading configurationsL %v", err)
+	}
+
+	s3Client, err := lib.NewS3Client(secrets.AwsRegionName, secrets.AwsAccessKey, secrets.AwsSecret)
+	if err != nil {
+		log.Fatalf("Failed to create s3 client: %v", err)
+	}
+
+	dbClient := lib.NewDynamoDBClient()
+	productStore := database.NewProductStore(dbClient)
+	productService := services.NewProductService(productStore, s3Client)
+
+	responseHandler := lib.NewResponseHandler[interface{}]()
+	productHandler := handlers.NewProductHandler(productService, responseHandler)
+
+	handlersMap := map[string]func(request events.APIGatewayProxyRequest) *events.APIGatewayProxyResponse{
+		"GET": func(request events.APIGatewayProxyRequest) *events.APIGatewayProxyResponse {
+			if _, ok := request.PathParameters["id"]; ok {
+				return productHandler.GetProductByID(request)
+			}
+			return productHandler.GetProducts(request)
+		},
+		"POST":   productHandler.CreateProduct,
+		"PUT":    productHandler.UpdateProduct,
+		"DELETE": productHandler.DeleteProduct,
+	}
+
+	if handler, exists := handlersMap[request.HTTPMethod]; exists {
+		return handler(request), nil
+	}
+
+	msg, _ := json.Marshal(map[string]string{
+		"message": "method not allowed",
+	})
+
+	return &events.APIGatewayProxyResponse{
+		StatusCode: http.StatusMethodNotAllowed,
+		Body:       string(msg),
+	}, nil
+}
