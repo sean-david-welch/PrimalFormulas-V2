4ed1315b27b52c321462649f151c6b38d7e869b9
seanwelch <seanwelch51199@icloud.com>
2024-05-18 10:12:49 +0100

    asset lambda handler

--- server/api/src/assets/main.go
+++ server/api/src/assets/main.go
@@ -1,1 +1,62 @@
 package main
+
+import (
+	"encoding/json"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/sean-david-welch/primal-formulas/config"
+	"github.com/sean-david-welch/primal-formulas/database"
+	"github.com/sean-david-welch/primal-formulas/handlers"
+	"github.com/sean-david-welch/primal-formulas/lib"
+	"github.com/sean-david-welch/primal-formulas/services"
+	"log"
+	"net/http"
+)
+
+func main() {
+	lambda.Start(Handler)
+}
+
+func Handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	secrets, err := config.NewSecrets()
+	if err != nil {
+		log.Fatalf("Error loading configurations: %v", err)
+	}
+
+	s3Client, err := lib.NewS3Client(secrets.AwsRegionName, secrets.AwsAccessKey, secrets.AwsSecret)
+	if err != nil {
+		log.Fatalf("Failed to create s3 client: %v", err)
+	}
+
+	dbClient := lib.NewDynamoDBClient()
+	assetStore := database.NewAssetStore(dbClient)
+	assetService := services.NewAssetStore(assetStore, s3Client)
+
+	responseHandler := lib.NewResponseHandler[interface{}]()
+	assetHandler := handlers.NewAssetHandler(assetService, responseHandler)
+
+	handlersMap := map[string]func(request events.APIGatewayProxyRequest) *events.APIGatewayProxyResponse{
+		"GET": func(request events.APIGatewayProxyRequest) *events.APIGatewayProxyResponse {
+			if _, ok := request.PathParameters["id"]; ok {
+				return assetHandler.GetAssetByID(request)
+			}
+			return assetHandler.GetAssets(request)
+		},
+		"POST":   assetHandler.CreateAsset,
+		"PUT":    assetHandler.UpdateAsset,
+		"DELETE": assetHandler.DeleteAsset,
+	}
+
+	if handler, exists := handlersMap[request.HTTPMethod]; exists {
+		return handler(request), nil
+	}
+
+	msg, _ := json.Marshal(map[string]string{
+		"message": "method not allowed",
+	})
+
+	return &events.APIGatewayProxyResponse{
+		StatusCode: http.StatusMethodNotAllowed,
+		Body:       string(msg),
+	}, nil
+}
